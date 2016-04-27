be7105c3ee5331fd301cfb92eb861cd67ffc55c4
hutchgrant <h.g.utchinson@gmail.com>
2016-04-26 20:47:03 -0400

    adding graph, verbose data page, updating style

--- ont_university_funding/script.js
+++ ont_university_funding/script.js
@@ -1,23 +1,39 @@
-var univFunding = angular.module('univFunding', ['ui-bootstrap']);
+angular.module('univFunding', [])
 	
-	function MyAppController($scope, $http, $sce)
-	{
+	.controller('FundController', function($scope, $http) {
 	
-	$http.get('./funding.json' ).then( function ( response ) {
-	    $scope.univ = response.data;
-	    
-	    $scope.trustedJsonRequest = function() {
-	      return $sce.trustAsHtml($scope.univ);
-	    };
+		$http.get('./funding.json' ).then( function ( response ) {
+		    $scope.univ = response.data;
+		    
+		    $scope.trustedJsonRequest = function() {
+		      return $sce.trustAsHtml($scope.univ);
+		    };
 
-	    $scope.totalUniv = $scope.univ.length;
-		$scope.title = 'Ontario University Provincial Funding';
-		$scope.description = 'All financial information on what each Ontario ministry pays to each university';
+		    $scope.totalUniv = $scope.univ.length;
+			$scope.title = 'Ontario University Provincial Funding';
+			$scope.description = 'All financial information on what each Ontario ministry pays to each university';
+			$scope.explanationTitle = "Methodology";
+			var method = "methodology text";
+			$scope.fundingExplanation = method;
 
-		$scope.clearFilter = function() {
-			$scope.query = '';
-		};
+			$scope.clearFilter = function() {
+				$scope.query = '';
+			};
 
+			$scope.methodology = function(){
+				$scope.explanationTitle = "Methodology";
+				$scope.fundingExplanation = "methodology text";
+			}
+
+			$scope.problems = function(){
+				$scope.explanationTitle = "Problems";
+				$scope.fundingExplanation = "problem text";
+			}
+
+			$scope.results = function(){
+				$scope.explanationTitle = "Results";
+				$scope.fundingExplanation = "result text";
+			}
+		});
+	
 	});
-	
-	}
